c5ec098c0ea6bc1c8d055f027b5477106580eb45
Sochoeun <sochoeun1202@gmail.com>
2024-06-08 18:14:40 +0700

    For Developmnet on Local

--- src/main/java/com/sochoeun/Application.java
+++ src/main/java/com/sochoeun/Application.java
@@ -7,33 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class Application {
-	//private final PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	/*@Bean
-	public CommandLineRunner commandLineRunner(
-			UserRepository userRepository,
-			RoleRepository roleRepository
-	) {
-		return args -> {
-			if (roleRepository.findByName("SUPER-ADMIN").isEmpty()) {
-				roleRepository.save(Role.builder().name("SUPER-ADMIN").build());
-			}
-
-			User super_admin = User.builder()
-					.firstname("SUPER")
-					.lastname("ADMIN")
-					.email("super-admin@gmail.com")
-					.password(passwordEncoder.encode("super-admin"))
-					.profile("")
-					.status(true)
-					.roles(roleRepository.findAll())
-					.build();
-			userRepository.save(super_admin);
-		};
-	}*/
 
 }
